8737dc29186203c689b9b9a15885881165f1f953
Suresux <100307594+Suresux@users.noreply.github.com>
2025-12-21 21:15:40 +0530

    Delete modules directory

--- modules/app_service/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "url" {
-  description = "App Service default hostname"
-  value       = azurerm_app_service.this.default_site_hostname
-}
--- modules/app_service_plan/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "id" {
-  description = "App Service Plan ID"
-  value       = azurerm_app_service_plan.this.id
-}
